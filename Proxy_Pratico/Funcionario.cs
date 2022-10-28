38290bf242ddcfbfb96527c4dfcba6503bfcea56
Dev-joao-script <Dev.joao.script@gmail.com>
2022-10-28 18:33:14 -0200

    Revisão de conteudo

---
+++ Proxy_Pratico/Funcionario.cs
@@ -0,0 +1,13 @@
+﻿namespace Proxy_Pratico
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Perfil { get; set; }
+        public Funcionario(string nome, string perfil)
+        {
+            Nome = nome;
+            Perfil = perfil;
+        }
+    }
+}
---
+++ Proxy_Pratico/PastaCompartilhadaProxy.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_Pratico
+{
+    //Proxy
+    //contém uma referência à classe RealSubject e pode acessar
+    //os membros da classe RealSubject conforme necessário. 
+    public class PastaCompartilhadaProxy : IPastaCompartilhada
+    {
+        private IPastaCompartilhada pasta;
+
+        private Funcionario funcionario;
+        public PastaCompartilhadaProxy(Funcionario funci)
+        {
+            funcionario = funci;
+        }
+
+        public void OperacaoDeLeituraGravacao()
+        {
+           if(funcionario.Perfil.ToUpper() == "CEO")
+           {
+                pasta = new PastaCompartilhada();
+
+                Console.WriteLine("O proxy 'Pasta Compartilhada' invoca a pasta Real" 
+                    + " : 'método usado : OperacaoDeLeituraGravacao()\n");
+
+                pasta.OperacaoDeLeituraGravacao();
+           }
+           else
+           {
+                Console.WriteLine("O proxy 'Pasta Compartilhada'  avisa : " +
+                         "'Você não tem permissão para acessar esta pasta'\n");
+           }
+        }
+    }
+}
